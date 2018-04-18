1ccc254adc0625a3db27370d2a75bb27a217e7b5
Ememobong <emembobbie@gmail.com>
2018-04-18 17:07:52 +0100

    my first project

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<?php Date_default_timezone_set('Africa/Lagos');?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    <h1>HNG-INTERNSHIP 4</h1><br />
+
+    <div class="time"><b>
+    <?php
+    echo date("h:i:s").'<br />';
+
+    echo date("d/m/Y")
+
+    ?>
+    </div>
+    
+
+
+
+</body>
+</html>
